54e6cfac5f59172759c3db7f273258f94930a688
flomess <florian.messu@gmail.com>
2022-04-08 11:58:52 +0200

    my team (not working)

--- Pages/MyTeam.js
+++ Pages/MyTeam.js
@@ -2,14 +2,42 @@ import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
 import { FlatList, SafeAreaView, StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
 import PokeCard from '../Components/PokeCard';
+import StorageService from '../Utils/StorageService';
 import {getPokemons} from '../Api/PokeApi';
-import StorageService from '../Utils/StorageService'
 export default function MyTeam(props){
 
+    const { navigation,  ...restProps } = props
+
+    const [listPokemon, setListPokemon] = StorageService.retrieveData('myTeam');
+
+    const renderItem = ({ item}) => {
+        return (
+            <PokeCard style={styles.container} url={item.url} name={item.name} navigation={navigation}></PokeCard>
+        )
+    }
 
     return (
         <SafeAreaView style={styles.container}>
-            suce
+            <View style={styles.header}>
+                <Text style={styles.title}>Pokedex</Text>
+            </View>
+            <View style={styles.list}>
+                {
+                    listPokemon.length > 0 ?
+                        <FlatList
+                            horizontal={false}
+                            numColumns={2}
+                            data={listPokemon}
+                            key={(item,index)=>index.toString()}
+                            renderItem={renderItem}
+                            keyExtractor={(item) => item.name}
+                        />
+                        :
+                        <Text>Aucun Pokémon dans la Team</Text>
+
+                }
+            </View>
+            <StatusBar style="auto" />
         </SafeAreaView>
     )
 }
